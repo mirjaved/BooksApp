ebd5ac7b34bd702cf7619845b814602e20c661ed
Javed Nasir Mir <javedmir@gmail.com>
2021-10-28 10:35:55 +0800

    Added Book Form Factory

--- module/Book/src/Form/Factory/BookFormFactory.php
+++ module/Book/src/Form/Factory/BookFormFactory.php
@@ -11,13 +11,13 @@ use Book\Entity\Author;
 /**
  * This is the factory for BookForm. Its purpose is to instantiate it.
  */
-class BookControllerFactory implements FactoryInterface
+class BookFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $repository = $container->get(Author::class);
         
         // Instantiate the controller and inject dependencies
-        return new BookController($entityManager, $bookManager);
+        return new BookFormFactory($entityManager, $bookManager);
     }
 }
